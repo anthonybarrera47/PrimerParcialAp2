6f66bf92ea37e346751ff00c2c83c4172474a53f
anthonybarrera47 <anthony_barrera@ucne.edu.do>
2019-09-21 20:13:59 -0400

    Agregando la DLL de la DAL

---
+++ DAL/Contexto.cs
@@ -0,0 +1,22 @@
+﻿using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class Contexto : DbContext
+    {
+        public Contexto() : base("ConStr")
+        { }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
